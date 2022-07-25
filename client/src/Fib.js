81ce2f291eb645228798bf7b31ab1a246a26dc1e
shiri2001 <shirile01@gmail.com>
2022-07-25 16:27:51 +0300

    confirm

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -75,3 +75,28 @@ class Fib extends Component {
 }
 
 export default Fib;
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={(event) => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
+}
+
+export default Fib;
